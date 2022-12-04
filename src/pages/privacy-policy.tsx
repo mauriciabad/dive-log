eec0935ff866d97244f87f1dfb826625bfbbc3a9
mauriciabad <hello@mauriciabad.com>
2022-12-04 23:28:32 +0800

    explain data deletion

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -8,6 +8,7 @@ const PrivacyPolicy: CustomNextPage = () => {
     <p>This app allows anyone to manage a regestry of their dives.</p>
     <p>The information we store is only visible to the owner user.</p>
     <p>We only use your information for processing and displaying it to you in the application.</p>
+    <p>To delete all your data just delete your account</p>
 
     <Link
       className="inline-block rounded-full bg-black/20 px-10 py-3 font-semibold no-underline transition hover:bg-black/30"
