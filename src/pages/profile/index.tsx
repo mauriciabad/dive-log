3abec02d1cc430d5c81b2d713ddd1a57eefe0b02
mauriciabad <hello@mauriciabad.com>
2022-12-04 19:39:07 +0800

    Better routes

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,51 @@
+import type { GetServerSideProps, NextPage } from "next";
+import { getSession, signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { trpc } from "../../utils/trpc";
+
+const User: NextPage = () => {
+  const { data: session } = useSession();
+  const { data: dives } = trpc.dive.getUserDives.useQuery()
+
+  return <>
+    <p>Welcome {session?.user?.name}!</p>
+    <button
+      className="rounded-full bg-black/10 px-10 py-3 font-semibold no-underline transition hover:bg-black/20"
+      onClick={session ? () => signOut() : () => signIn()}
+    >
+      {session ? "Sign out" : "Sign in"}
+    </button>
+
+    <h3 className="text-2xl font-bold">Dives list</h3>
+    <Link
+      className="inline-block rounded-full bg-black/10 px-10 py-3 font-semibold no-underline transition hover:bg-black/20"
+      href="/dives"
+    >
+      Dives page
+    </Link>
+
+    {!dives ? <p className="text-2xl">Loading dives...</p> : <ul>
+      {dives.map((dive) => <li key={dive.id}><span>{dive.date.toLocaleString()}</span> - <span>{dive.locationName}</span></li>)}
+    </ul>
+    }
+  </>
+}
+
+export default User;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
