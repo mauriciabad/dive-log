60ad7f9ec1911b8d5ce6351b15a49cafc2ca0f58
mauriciabad <hello@mauriciabad.com>
2022-12-04 23:49:00 +0800

    Add env keys to schema

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -21,6 +21,10 @@ export const serverSchema = z.object({
   ),
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+  FACEBOOK_CLIENT_ID: z.string(),
+  FACEBOOK_CLIENT_SECRET: z.string(),
 });
 
 /**
