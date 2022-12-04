5ebc6ac022856f7ecf9e9f32eb667386ca172cb9
mauriciabad <hello@mauriciabad.com>
2022-12-04 23:18:29 +0800

    Add rounded corners

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Use here: <https://divelog.vercel.app/>
 
 <p align="center">
-  <img height="256" src="docs/ai-images/1245137059_A_scuba_diver_writing_in_a_diary_underwater_in_a_coral_reef_in_a_3d_style.png" />
+  <img height="256" src="src/assets/brand-image-dound-corners.png" />
 </p>
 
 > **WORK IN PROGRESS**
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,7 +6,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
-import brandImage from '../../../assets/brand-image.png'
+import brandImage from '../../../assets/brand-image-rounded-corners.png'
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
