9feb446ce098dc17d62e9ffa6e68b0e1e82ea403
mauriciabad <hello@mauriciabad.com>
2022-12-06 01:19:53 +0800

    Add wave animation

--- src/pages/user/dives/index.tsx
+++ src/pages/user/dives/index.tsx
@@ -4,26 +4,35 @@ import { trpc } from "../../../utils/trpc";
 import type { CustomNextPage } from "../../_app";
 import FabButton from '../../../components/FabButton'
 import DiveCard from '../../../components/DiveCard'
+import WaveAnimation from '../../../components/WaveAnimation'
 
 const Dives: CustomNextPage = () => {
   const { data: dives } = trpc.dive.getUserDives.useQuery();
 
   return (
     <>
-      <div
-        className="grid grid-flow-row gap-4"
-        style={{ 'grid-template-columns': 'repeat(auto-fit, minmax(20rem, 1fr))' }}
-      >
-        {dives?.map((dive) =>
-          <DiveCard dive={dive} key={dive.id} />)
-          ??
-          <p className="text-2xl">Loading dives...</p>}
-      </div>
+      {dives ?
+        <>
+          <div
+            className="grid grid-flow-row gap-4"
+            style={{ 'grid-template-columns': 'repeat(auto-fit, minmax(20rem, 1fr))' }}
+          >
+            {dives.map((dive) =>
+              <DiveCard dive={dive} key={dive.id} />)
+            }
+          </div>
 
-      <div className="h-16 pt-6 box-content" />
-      <div className="fixed bottom-0 left-0 right-0 p-6 max-w-5xl mx-auto text-right">
-        <FabButton href="/user/dives/new" className="sm:h-24 sm:w-24" />
-      </div>
+          <div className="h-16 pt-6 box-content" />
+          <div className="fixed bottom-0 left-0 right-0 p-6 max-w-5xl mx-auto text-right">
+            <FabButton href="/user/dives/new" className="sm:h-24 sm:w-24" />
+          </div>
+        </> : <>
+          <div className="flex flex-col items-center justify-center h-96 overflow-hidden">
+            <WaveAnimation className="max-w-[125vw]" />
+            <p className="text-2xl">Loading...</p>
+          </div>
+        </>
+      }
     </>
   );
 };
