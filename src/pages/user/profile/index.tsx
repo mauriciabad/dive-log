c476745656e19def0797dbb9780db8d983da9682
mauriciabad <hello@mauriciabad.com>
2022-12-07 00:39:39 +0800

    Use buttons

--- src/pages/user/profile/index.tsx
+++ src/pages/user/profile/index.tsx
@@ -1,9 +1,10 @@
 import { signOut, useSession } from "next-auth/react";
-import Link from "next/link";
 import Image from "next/image";
 import loginRequired from "../../../utils/loginRequired";
 import { trpc } from "../../../utils/trpc";
 import type { CustomNextPage } from "../../_app";
+import { TbLogout, TbBook } from "react-icons/tb";
+import IconButton from "../../../components/IconButton";
 
 const Profile: CustomNextPage = () => {
   const { data: session } = useSession();
@@ -15,23 +16,21 @@ const Profile: CustomNextPage = () => {
   return <>
 
     <Image className="h-8 w-8 rounded-full" src={userImage} alt="" height={32} width={32} />
-    <p>Name : {session?.user?.name}</p>
-    <p>Email : {session?.user?.email}</p>
-    <button
-      className="inline-block rounded-full bg-black/10 px-10 py-3 font-semibold no-underline transition hover:bg-black/20"
-      onClick={() => signOut({ callbackUrl: '/' })}
-    >Sign out</button>
+    <p>Name: {session?.user?.name}</p>
+    <p>Email: {session?.user?.email}</p>
 
-    <h3 className="text-2xl font-bold">Dives list</h3>
-    <Link
-      className="inline-block rounded-full bg-black/10 px-10 py-3 font-semibold no-underline transition hover:bg-black/20"
-      href="/user/dives"
-    >
-      Dives page
-    </Link>
+    <IconButton
+      onClick={() => signOut({ callbackUrl: '/' })}
+      text="Sign out"
+      Icon={TbLogout} />
+
+
+    <h3 className="text-2xl font-bold mt-6">Dives list</h3>
+    <IconButton href="/user/dives" text="Go to my dives" Icon={TbBook} />
+
 
     {!dives ? <p className="text-2xl">Loading dives...</p> : <ul>
-      {dives.map((dive) => <li key={dive.id}><span>{dive.date.toLocaleString()}</span> - <span>{dive.locationName}</span></li>)}
+      {dives.map((dive) => <li key={dive.id}><span>#{dive.diveNumber}</span> at<span>{dive.startDateTime.toLocaleString()}</span></li>)}
     </ul>
     }
   </>
