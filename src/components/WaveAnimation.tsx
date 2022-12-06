7c3333e726f4cefdf1e4df291a8fa1a37bca64d0
mauriciabad <hello@mauriciabad.com>
2022-12-07 00:20:43 +0800

    Fix align 0 dives

--- src/components/WaveAnimation.tsx
+++ src/components/WaveAnimation.tsx
@@ -51,7 +51,7 @@ const WaveAnimation: FC<Props> = ({ className }) => {
   return (
     <svg className={classNames(className, "")} width="610" height="100" viewBox="0 0 610 100" fill="none" xmlns="http://www.w3.org/2000/svg" id="wave">
       <g ref={group}>
-        <path ref={path} stroke="currentColor" strokeWidth="6" strokeLinecap="round" strokeLinejoin="round" d="M5 75.0001C33.5 75.0001 16.5 25.0001 84 25.0001C49 42.0001 73 75.0001 105 75.0001C133.5 75.0001 116.5 25 184 25C149 42 173 75 205 75C233.5 75 216.5 25 284 25C249 42 273 75 305 75C333.5 75 316.5 25.0001 384 25.0001C349 42.0001 373 75.0001 405 75.0001C433.5 75.0001 416.5 25.0001 484 25.0001C449 42.0001 473 75.0001 505 75.0001C533.5 75 516.5 25 584 25C549 42 573 75 605 75" />
+        <path ref={path} stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" d="M5 75.0001C33.5 75.0001 16.5 25.0001 84 25.0001C49 42.0001 73 75.0001 105 75.0001C133.5 75.0001 116.5 25 184 25C149 42 173 75 205 75C233.5 75 216.5 25 284 25C249 42 273 75 305 75C333.5 75 316.5 25.0001 384 25.0001C349 42.0001 373 75.0001 405 75.0001C433.5 75.0001 416.5 25.0001 484 25.0001C449 42.0001 473 75.0001 505 75.0001C533.5 75 516.5 25 584 25C549 42 573 75 605 75" />
       </g>
     </svg>
   )
--- src/pages/user/dives/index.tsx
+++ src/pages/user/dives/index.tsx
@@ -3,7 +3,6 @@ import type { CustomNextPage } from "../../_app";
 import FabButton from '../../../components/FabButton'
 import DiveCard from '../../../components/DiveCard'
 import WaveAnimation from '../../../components/WaveAnimation'
-import Link from "next/link";
 import { TbPlus } from 'react-icons/tb'
 import loginRequired from "../../../utils/loginRequired";
 import IconButton from "../../../components/IconButton";
@@ -14,19 +13,20 @@ const Dives: CustomNextPage = () => {
     <>
       {dives ?
         <>
-          <div
-            className="grid grid-flow-row gap-4 [grid-template-columns:repeat(auto-fit,minmax(20rem,1fr))]"
-          >
-            {
-              dives.length
-                ? dives.map((dive) => <DiveCard dive={dive} key={dive.id} />)
-                :
-                <div className="flex flex-col items-center justify-center h-96">
-                  <p className="text-5xl mb-12 font-light text-gray-700">You have 0 dives</p>
-                  <IconButton href="/user/dives/new" text="Create your first dive" Icon={TbPlus} />
-                </div>
-            }
-          </div>
+          {
+            dives.length
+              ?
+              <div
+                className="grid grid-flow-row gap-4 [grid-template-columns:repeat(auto-fit,minmax(20rem,1fr))]"
+              >
+                {dives.map((dive) => <DiveCard dive={dive} key={dive.id} />)}
+              </div>
+              :
+              <div className="flex flex-col items-center justify-center h-96">
+                <p className="text-4xl sm:text-5xl mb-12 font-light text-gray-700 text-center">You have 0 dives</p>
+                <IconButton href="/user/dives/new" text="Create your first dive" Icon={TbPlus} />
+              </div>
+          }
 
           <div className="h-16 pt-6 box-content" />
           <div className="fixed bottom-0 left-0 right-0 p-6 max-w-5xl mx-auto text-right">
