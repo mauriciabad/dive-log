fcdf948786585d5c57b9999558deca6e07c2bfd6
mauriciabad <hello@mauriciabad.com>
2022-12-12 03:19:25 +0800

    fix duration parse

--- src/components/DiveCard.tsx
+++ src/components/DiveCard.tsx
@@ -59,6 +59,9 @@ function durationToString(durationInMs: number): string {
   const hours = Math.floor(durationInMs / (1000 * 60 * 60))
   const minutes = (durationInMs / (1000 * 60) % 60)
 
-  return `${hours && hours.toFixed(0) + 'h '}${(minutes).toFixed(0)}min`
+  const hoursText = hours > 0 ? `${hours.toFixed(0)}h` : ''
+  const minutesText = minutes > 0 ? `${(minutes).toFixed(0)}min` : ''
+
+  return [hoursText, minutesText].join(' ')
 }
 
